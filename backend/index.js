32100f804b21ecdeb57e84ed205ea0721a5166ff
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2026-01-03 16:38:55 +0530

    add changes to local repository

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://technologyterms.netlify.app",
+  "https://techterms.netlify.app/",
 ];
 app.use(cors({
   origin: function (origin, callback) {
